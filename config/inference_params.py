7163b044be02bf9094913cc58aaf66bcc283a296
Keren Cohen <keren.c91@gmail.com>
2025-04-27 10:35:19 +0300

    Remove duplicate activation param, remove redundant spaces

--- config/inference_params.py
+++ config/inference_params.py
@@ -12,39 +12,38 @@ class DictParams:
 
 # Example usage:
 inference_args = DictParams(
-    L = 24,
-    K = 2,
-    window_size = 6, 
-    depths = [6, 6],                      
-    num_heads = [2, 2], 
-    embed_dim = 256,
-    data_mode = 'random', # 'random' / 'fixed'
+    L=24,
+    K=2,
+    window_size=6, 
+    depths=[6, 6],                      
+    num_heads=[2, 2], 
+    embed_dim=256,
+    data_mode='random', # 'random' / 'fixed'
     data_size=100,
-    disable_transformers = False,
-    from_pretrained = False,
-    activation = 'LeakyReLU',
+    disable_transformers=False,
+    from_pretrained=False,
     sigma=0.,
     )
 
 inference_params = DictParams(
 
     
-    qkv_bias = False,
-    qk_scale = False,
-    drop = 0.,
-    attn_drop = 0.,
-    drop_path_rate = 0.1,
-    norm_layer = True,
-    downsample = False,
-    resi_connection = '1conv',
+    qkv_bias=False,
+    qk_scale=False,
+    drop=0.,
+    attn_drop=0.,
+    drop_path_rate=0.1,
+    norm_layer=True,
+    downsample=False,
+    resi_connection='1conv',
     patch_size=1, #'patch size used in training SwinIR. '
                   #'Just used to differentiate two different settings in Table 2 of the paper. '
                   #'Images are NOT tested patch by patch.'
-    pre_conv_channels = [8, 32], 
-    reduce_height = [4, 3, 3],      
-    pre_residuals = 11,
-    post_residuals = 14,   
-    activation = 'LeakyReLU',      
+    pre_conv_channels=[8, 32], 
+    reduce_height=[4, 3, 3],      
+    pre_residuals=11,
+    post_residuals=14,   
+    activation='LeakyReLU',      
     )
 
 
